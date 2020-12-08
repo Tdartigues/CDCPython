ea4b2a8c76d894845bee6f7ecdf6d69f2a6df75b
Zayed Charef <zayed.charef@gmail.com>
2020-12-08 11:04:15 +0100

    Feat(class-security part.1): added functionality

--- Classe/Car.py
+++ Classe/Car.py
@@ -1,3 +1,5 @@
+import Error
+
 class Car():
     def __init__(self, nom, referent, limite):
         self.nom = nom
@@ -5,6 +7,22 @@ class Car():
         self.limite = limite
         self.classes = []
         self.eleves = []
+        self.professeurs = []
 
     def ajout(self, eleve):
-        self.eleves.append(eleve)
+        if (len(self.eleves) < self.limite):
+            if len(self.eleves) > len(set(self.eleves)) and len(self.eleves) > 2:
+                raise Error.TooMuchClassesPerBusError
+            else:
+                self.eleves.append(eleve)
+        else:
+            raise Error.BusOverloadError
+
+    def start(self):
+        for classe in self.classes:
+            if (len(classe) > 0):
+                pass
+            else:
+                raise Error.BusIsEmptyError
+        if ((len(self.professeurs) / len(self.eleves)) != 0.1):
+            raise Error.NotEnoughTeacherError
